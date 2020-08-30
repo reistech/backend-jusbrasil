8e9141fb9c4655b893293648b47461b5f2663c22
lucas placido <lucas.placido.93@gmail.com>
2020-08-30 20:38:02 -0300

    feat: modificacao service

--- src/Services/ProcessoTribunalService.cs
+++ src/Services/ProcessoTribunalService.cs
@@ -5,10 +5,24 @@ using src.Models;
 
 namespace src.Services
 {
+    
     public class ProcessoTribunalService : IProcessoTribunalService
     {
+        const string urlTribunalAlagoas1grau = "https://www2.tjal.jus.br/cpopg/open.do";
+        const string urlTribunalAlagoas2grau = "https://www2.tjal.jus.br/cposg5/open.do";
+        const string urlTribunalMatoGrossoSul1grau = "https://esaj.tjms.jus.br/cpopg5/open.do";
+        const string urlTribunalMatoGrossoSul2grau = "https://esaj.tjms.jus.br/cposg5/open.do";
+
+
         public Processo ObterProcessoTribunal(string numeroProcesso)
         {
+            //padrão numero Processo
+
+
+            //Consulta em cada grau
+
+
+            //Chamada do crawller
             return new Processo();
         }
     }
